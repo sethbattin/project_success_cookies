81bbfc59cfb97d5aa52ed9fdf24201371244a5a7
Seth Battin <seth.battin@shipt.com>
2022-03-02 22:07:59 -0600

    make an express server

---
+++ server.js
@@ -0,0 +1,12 @@
+// it's just https://expressjs.com/en/starter/hello-world.html
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
